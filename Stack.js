35362c619afa88aefd958b0ed50548de7a8cfdb2
sekiguchi nagisa <skgchxngsxyz-work@linux-pshy.site>
2013-04-03 18:26:43 +0900

    add Qsort.js

--- Stack.js
+++ Stack.js
@@ -1,29 +1,24 @@
 var Stack = function() {
     this.value = 0;
     this.next = 0;
+
+    this.push = function(value) {
+        var temp = new Stack();
+        temp.value = value;
+        temp.next = this.next;
+        this.next = temp;
+    }
+
+    this.pop = function() {
+        var temp = this.next;
+        this.next = temp.next;
+        return temp.value;
+    }
 }
 
-var push = function(stack, value) {
-    var temp = new Stack();
-    temp.value = value;
-    temp.next = stack;
-    stack = temp;
-}
+var s = new Stack();
+s.push(1);
+s.push(2);
 
-var pop = function(stack) {
-    var temp = stack;
-    stack = temp.next;
-
-    return temp.value;
-}
-
-var main = function() {
-    var s = new Stack();
-    push(s, 1);
-    push(s, 2);
-
-    console.log(pop(s));
-    console.log(pop(s));
-}
-
-main();
+console.log(s.pop());
+console.log(s.pop());
