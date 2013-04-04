cba3de95aae1d73a7a70ecf989fb3a96cbda0d4a
sekiguchi nagisa <skgchxngsxyz-work@linux-pshy.site>
2013-04-04 15:56:10 +0900

    several bugfix at HashMap.js

--- HashMap.js
+++ HashMap.js
@@ -1,17 +1,17 @@
 var HashMap = function() {
-    this.list = new Array(16);
-    this.put = function(key, value) {
+    this.arrayOfList = new Array(16);
+    this.set = function(key, value) {
         var hash = makeHash(key);
         console.log("hash = " + hash);
-        if (this.list[hash].value == null) {
-            this.list[hash].key = key;
-            this.list[hash].value = value;
+        if (this.arrayOfList[hash].value == null) {
+            this.arrayOfList[hash].key = key;
+            this.arrayOfList[hash].value = value;
         } else {
             var tempList = new List();
             tempList.key = key;
             tempList.value = value;
-            tempList.next = this.list[hash].next;
-            this.list[hash].next = tempList;
+            tempList.next = this.arrayOfList[hash].next;
+            this.arrayOfList[hash].next = tempList;
         }
     }
 
@@ -19,11 +19,11 @@ var HashMap = function() {
         var hash = makeHash(key);
         console.log("hash = " + hash);
 
-        if (this.list[hash].key == key) {
-            return this.list[hash].value;
+        if (this.arrayOfList[hash].key == key) {
+            return this.arrayOfList[hash].value;
         }
         
-        var tempList = this.list[hash].next;
+        var tempList = this.arrayOfList[hash].next;
         while (tempList != null) {
             if (tempList.key == key) {
                 return tempList.value;
@@ -34,8 +34,18 @@ var HashMap = function() {
         return null;
     }
 
+    var makeHash = function(key) {
+        var temp = 0;
+        for (var i = 0; i < key.length; i++) {
+            var c = key.charCodeAt(i);
+            temp += c;
+        }
+        var hash = temp % 16;
+        return hash;
+    }
+
     for (var i = 0; i < 16; i++) {
-        this.list[i] = new List();
+        this.arrayOfList[i] = new List();
     }
 }
 
@@ -45,20 +55,10 @@ var List = function() {
     this.next = null;
 }
 
-var makeHash = function(key) {
-    var temp = 0;
-    for (var i = 0; i < key.length; i++) {
-        var c = key.charCodeAt(i);
-        temp += c;
-    }
-    var hash = temp % 16;
-    return hash;
-}
-
 var hashMap = new HashMap();
-hashMap.put("hello world", 123);
-hashMap.put("stderr", 444);
-hashMap.put("world hello", 567);
+hashMap.set("hello world", 123);
+hashMap.set("stderr", 444);
+hashMap.set("world hello", 567);
 
 console.log(hashMap.get("hello world"));
 console.log(hashMap.get("stderr"));
